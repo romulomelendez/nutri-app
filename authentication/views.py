49c6836089dc03651f853c90e752f92652bb82fb
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-07-06 10:16:04 -0300

    creating a new routes

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,11 @@
+import re
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def register(request):
+    print(request)
+    return HttpResponse('This is my response')
+
+def login(request):
+    return HttpResponse('This is my login message')
