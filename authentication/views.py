20a1879851302b881e1057e1fdbe81e5e1f5f106
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-07-07 17:32:19 -0300

    creating a templates to render

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,9 @@
-import re
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def register(request):
-    print(request)
-    return HttpResponse('This is my response')
+    return render(request, 'register.html')
 
 def login(request):
-    return HttpResponse('This is my login message')
+    return render(request, 'login.html')
